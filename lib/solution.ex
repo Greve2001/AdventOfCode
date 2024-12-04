b5b6adca1898af9184ecbf58cbe18f09e94e08b9
Greve2001 <frederik.greve@gmail.com>
2024-12-04 11:23:13 +0100

    Included execution time

--- lib/solution.ex
+++ lib/solution.ex
@@ -8,8 +8,14 @@ defmodule Solution do
       @behaviour Solution
 
       def run() do
-        IO.write("Part 1: " <> to_string(part1()) <> "\n")
-        IO.write("Part 2: " <> to_string(part2()) <> "\n")
+        {p1_time, p1_res} = :timer.tc(fn -> part1() end)
+        {p2_time, p2_res} = :timer.tc(fn -> part2() end)
+
+        p1_time = Float.round(p1_time / 1000, 2)
+        p2_time = Float.round(p2_time / 1000, 2)
+
+        IO.write("Part 1 - (#{p1_time}ms): \t #{to_string(p1_res)}\n")
+        IO.write("Part 2 - (#{p2_time}ms): \t #{to_string(p2_res)}\n")
       end
     end
   end
